6a81683db05f9775c4f797772e1061c43061b2cd
ProdPreva1l <prod.preva1l@gmail.com>
2024-09-08 14:31:40 +1000

    fix users not getting removed from data on velocity shutdown

--- Velocity/src/main/java/us/dxtrus/api/APIVelocityBootstrapper.java
+++ Velocity/src/main/java/us/dxtrus/api/APIVelocityBootstrapper.java
@@ -3,14 +3,13 @@ package us.dxtrus.api;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
-import us.dxtrus.api.database.DatabaseManager;
-import us.dxtrus.api.models.user.User;
+import us.dxtrus.commons.utils.StringUtils;
 
 import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
 
 @Plugin(id = "api")
 public class APIVelocityBootstrapper {
@@ -24,13 +23,11 @@ public class APIVelocityBootstrapper {
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         proxy.getEventManager().register(this, new PlayerListeners());
-//
-//        proxy.getScheduler().buildTask(this, () -> {
-//            DatabaseManager.getInstance().getAll(User.class).thenAccept(users -> {
-//                for (User user : users) {
-//                    DatabaseManager.getInstance().delete(User.class, user);
-//                }
-//            });
-//        }).repeat(60L, TimeUnit.MINUTES).schedule();
+
+    }
+
+    @Subscribe
+    public void onProxyShutdown(ProxyShutdownEvent event) {
+        proxy.getAllPlayers().forEach(player -> player.disconnect(StringUtils.modernMessage("&cThis proxy is restarting...")));
     }
 }
